fa3976cd00b628c0ed3b21fa985cfe69849d772b
bedirhan <celayirbedirhan@gmail.com>
2023-02-12 13:45:46 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'aws-sdk';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +11,17 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
+  const configDocs = new DocumentBuilder()
+    .setTitle('Education API')
+    .setDescription('Education API description')
+    .setVersion('1.0')
+    .addTag('education')
+    .build();
+  const document = SwaggerModule.createDocument(app, configDocs);
+  SwaggerModule.setup('docs', app, document);
+
   app.setGlobalPrefix('v1');
+
   config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
