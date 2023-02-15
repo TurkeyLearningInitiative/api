25deca11caf0b8fa17083d4f9225a4c3d4d0841b
Zainab Lawal <zainablaw1012@gmail.com>
2023-02-15 18:27:27 +0300

    Fixed typo on app service and controller test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Turkey Learning Initiative API"', () => {
+      expect(appController.getHello()).toBe('Turkey Learning Initiative API');
     });
   });
 });
